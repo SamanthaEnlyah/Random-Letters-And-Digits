0cdd72df8a48bf7dade90e8cbaf3f8d0c7264e8d
SamanthaEnlyah <samantha.enlyah@hotmail.com>
2025-06-25 21:25:44 +0200

    Deleted comments containing link to non exisitng variable.

--- app/src/main/java/com/amethyst/randomlettersanddigits/ManualModeFragment.java
+++ app/src/main/java/com/amethyst/randomlettersanddigits/ManualModeFragment.java
@@ -11,11 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ManualModeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ManualModeFragment extends Fragment {
 
 
